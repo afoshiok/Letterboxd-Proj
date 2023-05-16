ecc59c34ce1e0b2e96e289961674143f17155488
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-05-16 12:29:46 -0500

    Starting on the web scraper!

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
-const test: String = "Hello World"
-console.log(test)
+import axios, { Axios } from "axios";
+
+const LetterboxdUser = 'FavourOshio' //My Letterboxd username for test
+const diary = `https://letterboxd.com/${LetterboxdUser}/films/diary/`
+
+const AxiosIntstance: Axios = axios.create()
+
+AxiosIntstance.get(diary)
+.then(
+    response => {
+        const html = response.data
+        console.log(html)
+    }
+)
+.catch(console.error)
