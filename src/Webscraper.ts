38b8105bccc1b43026efa4a4b29e7e96377df9b3
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-05-30 14:00:14 -0500

    FIXME: Issues with creating a new slash command

--- src/Webscraper.ts
+++ src/Webscraper.ts
@@ -2,38 +2,40 @@ import axios, { Axios } from "axios";
 import * as cheerio from 'cheerio';
 import { FilmData } from "src/types/letterboxd";
 
-const LetterboxdUser = 'FavourOshio' //My Letterboxd username for test
-const diary = `https://letterboxd.com/${LetterboxdUser}/films/diary/for/2023` //Just films for 2023
+export function LetterboxdData(username: string) {
+    const LetterboxdUser = username //My Letterboxd username for test
+    const diary = `https://letterboxd.com/${LetterboxdUser}/films/diary/for/2023` //Just films for 2023
 
 
-const AxiosIntstance: Axios = axios.create()
+    const AxiosIntstance: Axios = axios.create()
 
-AxiosIntstance.get(diary)
-.then(
-    response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const filmsTable = $('tbody').children('tr')
-        // console.log(filmsTable)
+    AxiosIntstance.get(diary)
+    .then(
+        response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const filmsTable = $('tbody').children('tr')
+            // console.log(filmsTable)
 
-        const films: FilmData[] = []
-        filmsTable.each((_i, elem) => {
-            const title: string = $(elem).find('.td-film-details').find('h3').text()
-            const userRating: number = parseInt($(elem).find('input.rateit-field').val())
-            const releaseDate: number = parseInt($(elem).find('.td-released').text())
-            const liked: boolean =  $(elem).find('.icon-liked').length > 0
-            const rewatched: boolean = $(elem).find('.td-rewatch.center.icon-status-off').length === 0
+            const films: FilmData[] = []
+            filmsTable.each((_i, elem) => {
+                const title: string = $(elem).find('.td-film-details').find('h3').text()
+                const userRating: number = parseInt($(elem).find('input.rateit-field').val())
+                const releaseDate: number = parseInt($(elem).find('.td-released').text())
+                const liked: boolean =  $(elem).find('.icon-liked').length > 0
+                const rewatched: boolean = $(elem).find('.td-rewatch.center.icon-status-off').length === 0
 
-            films.push({
-                title,
-                userRating,
-                releaseDate,
-                liked,
-                rewatched
+                films.push({
+                    title,
+                    userRating,
+                    releaseDate,
+                    liked,
+                    rewatched
+                })
             })
-        })
 
-        console.log(films)
-    }
-)
-.catch(console.error)
+            console.log(films)
+        }
+    )
+    .catch(console.error)
+}
