8564c56f5ee87025fc454e4e48981c9d8ae4347d
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-06-15 12:06:02 -0500

    NUKE!

--- src/Commands.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "./Command";
-import { Hello } from "./commands/hello";
-import { Films } from "./commands/films";
-
-export const Commands: Command[] = [Hello, Films]
--- src/types/letterboxd.ts
+++
@@ -1,7 +0,0 @@
-export interface FilmData {
-    title: string,
-    userRating: number
-    releaseDate: number,
-    liked: boolean,
-    rewatched: boolean
-}
