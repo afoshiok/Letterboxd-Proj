1d416b002024d42d4d6b27975cc3d6827f60988c
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-06-15 13:16:13 -0500

    Discord init

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,3 +1,17 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config'
+
 import { LetterboxdData } from "./webScraper";
 
-LetterboxdData("FavourOshio")
+
+const token = process.env.TOKEN
+
+const client = new Client({ intents: [[GatewayIntentBits.Guilds]]})
+
+client.on('ready', c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(token)
+
+// LetterboxdData("FavourOshio")
